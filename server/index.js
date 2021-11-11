4795daf4d35f033cc7717c0e6c02f66b2995fcf0
yanmby <gabrielegonzalez98@gmail.com>
2021-11-11 14:35:30 -0500

    Cc hidden

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://Yanmby:Neopets123@cluster0.teekb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://[Usuario][Password]@cluster0.teekb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
